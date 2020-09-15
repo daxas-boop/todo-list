8e09645fac32944eca04be5de356fdd0c40bd515
daxas-boop <daxas.swd@hotmail.com>
2020-09-14 22:52:03 -0300

    Creates new modules

--- src/storage.js
+++
@@ -1,9 +0,0 @@
-export function populateStorage(projects) {
-    localStorage.setItem('projects', JSON.stringify(projects))
-}
-
-export function getStorage() {
-    let retrievedProjects = localStorage.getItem('projects');
-    retrievedProjects = JSON.parse(retrievedProjects);
-    return retrievedProjects;
-}
