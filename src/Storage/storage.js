fb719dae85be536c629d96bcba93e2f1f1018a0f
daxas-boop <daxas.swd@hotmail.com>
2020-09-15 21:24:07 -0300

    Removes import and passes it as a parameter. Aplies arbnb styles

--- src/Storage/storage.js
+++ src/Storage/storage.js
@@ -1,23 +1,21 @@
-import {projects} from '../index.js'
-
 export function populateStorage(projects) {
-    localStorage.setItem('projects', JSON.stringify(projects))
+  localStorage.setItem('projects', JSON.stringify(projects));
 }
 
 export function getStorage() {
-    let retrievedProjects = localStorage.getItem('projects');
-    retrievedProjects = JSON.parse(retrievedProjects);
-    return retrievedProjects;
+  let retrievedProjects = localStorage.getItem('projects');
+  retrievedProjects = JSON.parse(retrievedProjects);
+  return retrievedProjects;
 }
 
-export function pushStorageToProjects(storageProjects) {
-    if(storageProjects){
-        storageProjects.forEach(storageProject => {
-            let project = projects.createProject(storageProject.title, storageProject.active);
-            
-            storageProject.todosArray.forEach( todo => {
-                project.createTodo(todo.title, todo.description, todo.dueDate, todo.priority);
-            });
-        });
-    }
+export function pushStorageToProjects(storageProjects, projects) {
+  if (storageProjects) {
+    storageProjects.forEach((storageProject) => {
+      const project = projects.createProject(storageProject.title, storageProject.active);
+
+      storageProject.todosArray.forEach((todo) => {
+        project.createTodo(todo.title, todo.description, todo.dueDate, todo.priority);
+      });
+    });
+  }
 }
