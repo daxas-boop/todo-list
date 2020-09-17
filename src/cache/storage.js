42c4a635ef3366f1da1d20b6353bc29d30215322
daxas-boop <daxas.swd@hotmail.com>
2020-09-16 22:45:16 -0300

    Removes the logic functions not related with cache

---
+++ src/cache/storage.js
@@ -0,0 +1,9 @@
+export function populateStorage(projects) {
+  localStorage.setItem('projects', JSON.stringify(projects));
+}
+
+export function getStorage() {
+  let retrievedProjects = localStorage.getItem('projects');
+  retrievedProjects = JSON.parse(retrievedProjects);
+  return retrievedProjects;
+}
